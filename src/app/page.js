5e98e200f21bda5814456d4395f57c554e9886e3
Pranav <pranavkumar192192@gmail.com>
2024-03-21 22:33:12 +0530

    Shadcn added

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen items-center justify-center flex">
+      <Button  variant="outline">BESTTT FULL STACK DEV = PRANAV KUMAR!!!!!!!!</Button>
+    </div>
+  );
+}
